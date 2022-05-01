6c40930e3440a3bc480f8bd3a0552d56fa591d5d
Vedvardhan <vedvardhan.g@gmail.com>
2022-05-01 22:56:18 +0530

    Payment gateway integration complete

---
+++ routes/payments.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const Razorpay = require("razorpay")
+const crypto = require("crypto")
+require("dotenv").config()
+
+const router = express.Router()
+
+router.post("/orders", (req, res) => {
+    const { amount, currency, receipt } = req.body
+    console.log("Creating RZP order of 2000rs")
+    try {
+        let instance = new Razorpay({
+            key_id: process.env.RZP_ID,
+            key_secret: process.env.RZP_KEY,
+        });
+        let options = {
+            amount, 
+            currency,
+            receipt
+        };
+        instance.orders.create(options, function(err, order) {
+            res.send({order})
+        }); 
+    }
+    catch(error) {
+        console.log(error)
+        res.send({error: "Error"})
+    }
+})
+
+router.post("/verify", (req, res) => {
+    console.log("Starting payment verification process")
+    body = req.body.razorpayOrderId + "|" + req.body.razorpayPaymentId;
+    let expectedSignature = crypto
+    .createHmac("sha256", process.env.RZP_KEY)
+    .update(body.toString())
+    .digest("hex");
+    var response = { status: "failure" };
+    if (expectedSignature === req.body.razorpaySignature) {
+        response = { status: "success" };
+    }
+    res.send(response)
+})
+module.exports = router
+
+
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const paymentRoutes = require("./routes/payments")
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/payment", paymentRoutes)
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Process runnning in port 5000")
+})
