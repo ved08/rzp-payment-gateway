86df00f16734dffd309fdd37c847abb5b0f40531
Vedvardhan <vedvardhan.g@gmail.com>
2022-05-07 21:37:25 +0530

    production debugging

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const paymentRoutes = require("./routes/payments")
 app.use(express.json())
 app.use(cors())
 
-app.use("/payment", paymentRoutes)
+// app.use("/payment", paymentRoutes)
+app.get('/', (req, res) => {
+    res.send("Server working")
+})
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Process runnning in port 5000")
+    console.log(`Process runnning in port ${process.env.PORT || 5000}`)
 })
